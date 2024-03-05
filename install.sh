d21bfc2b41eca3b6b3f1bf5941671b3585d0f252
OutLand <hatanduy123@gmail.com>
2024-03-05 15:49:44 +0700

    duy53-8

--- install.sh
+++ install.sh
@@ -20,11 +20,11 @@ install_3proxy() {
     sudo mkdir -p /usr/local/etc/3proxy/bin
     sudo mkdir -p /usr/local/etc/3proxy/logs
     sudo mkdir -p /usr/local/etc/3proxy/stat
-    sudo cp src/3proxy.c /usr/local/etc/3proxy/bin/
+    sudo cp src/3proxy* /usr/local/etc/3proxy/bin/
     sudo cp ./scripts/rc.d/proxy.sh /usr/local/etc/init.d/3proxy
     sudo chmod +x /usr/local/etc/init.d/3proxy
     sudo systemctl enable 3proxy
-    sudo cd $WORKDIR
+    cd $WORKDIR
 }
 
 install_3proxy
